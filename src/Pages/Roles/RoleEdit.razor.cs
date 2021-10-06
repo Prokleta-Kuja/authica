e0a6fa0cd0d84d3272e43c76e532f799b82179d7
Prokleta-Kuja <Prokleta.Kuja@gmail.com>
2021-10-06 09:28:36 +0000

    Roles crud

--- src/Pages/Roles/RoleEdit.razor.cs
+++ src/Pages/Roles/RoleEdit.razor.cs
@@ -12,26 +12,36 @@ using Microsoft.JSInterop;
 
 namespace authica.Pages.Roles
 {
-    public partial class RoleEdit
+    public partial class RoleEdit : IDisposable
     {
         [Inject] private IJSRuntime JS { get; set; } = null!;
         [Inject] private NavigationManager Nav { get; set; } = null!;
-        [Inject] private AppDbContext Db { get; set; } = null!;
         [Inject] private IPasswordHasher Hasher { get; set; } = null!;
+        [Inject] private IDbContextFactory<AppDbContext> DbFactory { get; set; } = null!;
         [Parameter] public Guid AliasId { get; set; }
+        private AppDbContext _db = null!;
         private RoleCreateModel? _create;
         private RoleEditModel? _edit;
         private Role? _item;
         private HashSet<string> _names = new();
+        private Guid? _selectedApp;
+        private Guid? _selectedUser;
+        private Dictionary<Guid, Entities.App> _roleApps = new();
+        private Dictionary<Guid, User> _roleUsers = new();
+        private Dictionary<Guid, Entities.App> _allApps = new();
+        private Dictionary<Guid, User> _allUsers = new();
         private Dictionary<string, string>? _errors;
         private readonly IRoles _t = LocalizationFactory.Roles();
+
+        protected override void OnInitialized() { _db = DbFactory.CreateDbContext(); base.OnInitialized(); }
+        public void Dispose() => _db?.Dispose();
 
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
             if (!firstRender)
                 return;
 
-            var roles = await Db.Roles.ToListAsync();
+            var roles = await _db.Roles.ToListAsync();
 
             if (AliasId == Guid.Empty)
                 _create = new();
@@ -43,9 +53,37 @@ namespace authica.Pages.Roles
                     _edit = new(_item);
             }
 
+
             foreach (var role in roles)
                 if (role.AliasId != AliasId)
                     _names.Add(role.Name.ToUpper());
+
+            _allApps = await _db.Apps
+                .Include(r => r.AppRoles)
+                .OrderBy(r => r.Name)
+                .ToDictionaryAsync(r => r.AliasId);
+
+            if (_item != null)
+                foreach (var app in _allApps)
+                    if (app.Value.AppRoles!.Any(ur => ur.RoleId == _item.RoleId))
+                        _roleApps.Add(app.Key, app.Value);
+
+
+            foreach (var app in _roleApps)
+                _allApps.Remove(app.Key);
+
+            _allUsers = await _db.Users
+                .Include(r => r.UserRoles)
+                .OrderBy(r => r.Email)
+                .ToDictionaryAsync(r => r.AliasId);
+
+            if (_item != null)
+                foreach (var user in _allUsers)
+                    if (user.Value.UserRoles!.Any(ur => ur.RoleId == _item.RoleId))
+                        _roleUsers.Add(user.Key, user.Value);
+
+            foreach (var user in _roleUsers)
+                _allUsers.Remove(user.Key);
 
             StateHasChanged();
         }
@@ -62,8 +100,8 @@ namespace authica.Pages.Roles
             _item = new(_create.Name!);
             _item.Disabled = _create.Disabled ? DateTime.UtcNow : null;
 
-            Db.Roles.Add(_item);
-            await Db.SaveChangesAsync();
+            _db.Roles.Add(_item);
+            await _db.SaveChangesAsync();
 
             Nav.NavigateTo(C.Routes.RolesFor(_item.AliasId));
             _create = null;
@@ -86,7 +124,59 @@ namespace authica.Pages.Roles
             else if (_item.Disabled.HasValue && !_edit.Disabled)
                 _item.Disabled = null;
 
-            await Db.SaveChangesAsync();
+            await _db.SaveChangesAsync();
+        }
+        void AddApp()
+        {
+            if (!_selectedApp.HasValue || !_allApps.TryGetValue(_selectedApp.Value, out var app))
+                return;
+
+            app.AppRoles!.Add(new AppRole { RoleId = _item!.RoleId });
+
+            _roleApps.Add(app.AliasId, app);
+            _allApps.Remove(app.AliasId);
+            _selectedApp = null;
+
+            StateHasChanged();
+        }
+        void RemoveApp(Guid roleAliasId)
+        {
+            if (!_roleApps.TryGetValue(roleAliasId, out var app))
+                return;
+
+            var appRole = app.AppRoles!.Single(ur => ur.RoleId == _item!.RoleId);
+            app.AppRoles!.Remove(appRole);
+
+            _allApps.Add(app.AliasId, app);
+            _roleApps.Remove(app.AliasId);
+
+            StateHasChanged();
+        }
+        void AddUser()
+        {
+            if (!_selectedUser.HasValue || !_allUsers.TryGetValue(_selectedUser.Value, out var user))
+                return;
+
+            user.UserRoles!.Add(new UserRole { RoleId = _item!.RoleId });
+
+            _roleUsers.Add(user.AliasId, user);
+            _allUsers.Remove(user.AliasId);
+            _selectedUser = null;
+
+            StateHasChanged();
+        }
+        void RemoveUser(Guid userAliasId)
+        {
+            if (!_roleUsers.TryGetValue(userAliasId, out var user))
+                return;
+
+            var userRole = user.UserRoles!.Single(ur => ur.RoleId == _item!.RoleId);
+            user.UserRoles!.Remove(userRole);
+
+            _allUsers.Add(user.AliasId, user);
+            _roleUsers.Remove(user.AliasId);
+
+            StateHasChanged();
         }
     }
 }
--- src/Translations/Pages/Roles.cs
+++ src/Translations/Pages/Roles.cs
@@ -5,35 +5,47 @@ namespace authica.Translations
         string ListTitle { get; }
         string AddTitle { get; }
         string EditTitle { get; }
+        string AppsTitle { get; }
+        string UsersTitle { get; }
         string TableId { get; }
         string TableName { get; }
         string TableDisabled { get; }
         string RowDisabled { get; }
         string Name { get; }
         string Disabled { get; }
+        string ChooseApp { get; }
+        string ChooseUser { get; }
     }
     public class Roles_en : Standard_en, IRoles
     {
         public string ListTitle => "Roles";
         public string AddTitle => "Add Role";
         public string EditTitle => "Edit Role";
+        public string AppsTitle => "Apps";
+        public string UsersTitle => "Users";
         public string TableId => "Id";
         public string TableName => "Name";
         public string TableDisabled => "Disabled";
         public string RowDisabled => "No";
         public string Name => "Name";
         public string Disabled => "Disabled";
+        public string ChooseApp => "Choose app";
+        public string ChooseUser => "Choose user";
     }
     public class Roles_hr : Standard_hr, IRoles
     {
         public string ListTitle => "Uloge";
         public string AddTitle => "Dodaj ulogu";
         public string EditTitle => "Izmijeni ulogu";
+        public string AppsTitle => "Appovi";
+        public string UsersTitle => "Korisnici";
         public string TableId => "Šifra";
         public string TableName => "Naziv";
         public string TableDisabled => "Onemogućeno";
         public string RowDisabled => "Ne";
         public string Name => "Naziv";
         public string Disabled => "Onemogućena";
+        public string ChooseApp => "Odaberi app";
+        public string ChooseUser => "Odaberi korisnika";
     }
 }
